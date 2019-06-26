c3023e02e564b84ad544eb2528d0877c8d5c00e4
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-26 13:09:43 -0300

    64. Implementando Curar

--- app.js
+++ app.js
@@ -15,9 +15,32 @@ new Vue({
       this.running = true;
       this.playerLife = 100;
       this.monsterLife = 100;
+    },
+    attack(especial) {
+      this.hurt('playerLife',7, 12, false);
+      this.hurt('monsterLife',5, 10, especial);
+    },
+    hurt(prop, min, max, especial) {
+      const plus = especial ? 5 : 0;
+      const hurt = this.getRandom(min + plus, max + plus);
+      this[prop] = Math.max(this.playerLife - hurt, 0);
+    },
+    healAndHurt(){
+      this.heal(10, 15);
+      this.hurt('playerLife', 7, 12, false);
+    },
+    heal(min, max) {
+      const heal = this.getRandom(min, max);
+      this.playerLife = Math.min(this.playerLife + heal, 100);
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min;
+      return Math.round(value);
     }
   },
   watch: {
-
+    hasResult(value) {
+      if(value) this.running = false;
+    }
   }
 });
