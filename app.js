1a2c7699b6f72cd276cd6bf3535a9cbc49d89f45
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-26 12:36:56 -0300

    62. Iniciar Jogo & Desistir do Jogo

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ new Vue({
   },
   computed: {
     hasResult() {
-      return this.playerLife == 0 || this.monsterLife == 0
+      return this.playerLife == 0 || this.monsterLife == 0;
     }
   },
   methods: {
-
+    startGame() {
+      this.running = true;
+      this.playerLife = 100;
+      this.monsterLife = 100;
+    }
   },
   watch: {
 
